dd2d3e4e87d68778067da0919c26ac386419f919
josh-leebrowne <90419150+josh-leebrowne@users.noreply.github.com>
2021-11-07 01:36:06 -0600

    Add files via upload

---
+++ week1/SelectingElements/SelectingElements.js
@@ -0,0 +1,10 @@
+console.log(document);
+
+
+const H1_ID = document.getElementById("h1ID");
+console.log(H1_ID);
+
+var h1 = document.createElement("h1ID");
+h1.textContent = "I was created in Java";
+
+
